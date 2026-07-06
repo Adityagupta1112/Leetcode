67ccf1b588185819db209c7feaf409f8f90eeff4
Aditya Gupta <adityagupta99904671@gmail.com>
2026-07-06 11:34:51 +0530

    Time: 13 ms (12.32%) | Memory: 16.6 MB (9.14%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -4,21 +4,17 @@ public:
         set<pair<int,int>>s;
         sort(intervals.begin(),intervals.end());
         int n=intervals.size(); 
-        for(auto& interval:intervals){
-            pair<int,int>p={interval[0],interval[1]};
-            s.insert(p);
-        }
         for(int i=0;i<n;i++){
             pair<int,int>p={intervals[i][0],intervals[i][1]};
-            if(s.find(p)==s.end()){
+            if(s.find(p)!=s.end()){
                 continue;
             }
             for(int j=0;j<n;j++){
                 if(i!=j && (intervals[i][0]<=intervals[j][0] && intervals[i][1]>=intervals[j][1])){
-                    s.erase(make_pair(intervals[j][0],intervals[j][1]));
+                    s.insert(make_pair(intervals[j][0],intervals[j][1]));
                 }
             }
         }
-        return s.size();
+        return n-s.size();
     }
 };
