d5a96059cad22d0fff0813a01de9bad9e834307f
Aditya Gupta <adityagupta99904671@gmail.com>
2026-07-07 11:03:20 +0530

    Time: 3 ms (7.74%) | Memory: 9.2 MB (42.26%) - LeetSync

---
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        vector<int>arr;
+        while(n>99){
+            int rem=n%10;
+            n=n/10;
+            arr.push_back(rem);
+        }
+        int rem=n%10;
+        n=n/10;
+        arr.push_back(rem);
+        arr.push_back(n);
+        reverse(arr.begin(),arr.end());
+        long long x=0;
+        long long sum=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0){
+                continue;
+            }
+            sum+=arr[i];
+            x*=10;
+            x+=arr[i];
+        }
+        return x*sum;
+    }
+};
