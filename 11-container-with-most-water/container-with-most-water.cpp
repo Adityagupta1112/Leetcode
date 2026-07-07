7dcc57d5b938f0535e920c23d7a4b860d1eede35
Aditya Gupta <adityagupta99904671@gmail.com>
2026-07-07 12:42:00 +0530

    Time: 3 ms (34.54%) | Memory: 62.9 MB (48.74%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    int ans=0;
-    int leftmax=0;
-    int rightmax=0;
-    void helper(int leftIdx,int rightIdx){
-        if(leftIdx!=-1 && rightIdx!=-1){
-            int breath=min(leftmax,rightmax);
-            int length=rightIdx-leftIdx;
-            int area=breath*length;
-            ans=max(ans,area);
-        }
-    }
     int maxArea(vector<int>& height) {
         int n=height.size();
         int i=0;
         int j=n-1;
-        int leftmaxIdx=-1;
-        int rightmaxIdx=-1;
+        int ans=0;
+        int leftmax=0;
+        int rightmax=0;
         while(i<=j){
+            leftmax=max(leftmax,height[i]);
+            rightmax=max(rightmax,height[j]);
             if(leftmax<rightmax){
-                if(leftmax<height[i]){
-                    leftmax=height[i];
-                    leftmaxIdx=i;
-                }
-                helper(leftmaxIdx,rightmaxIdx);
+                ans=max(ans,leftmax*(j-i));
                 i++;
             }
             else{
-                if(rightmax<height[j]){
-                    rightmax=height[j];
-                    rightmaxIdx=j;
-                }
-                helper(leftmaxIdx,rightmaxIdx);
+                ans=max(ans,rightmax*(j-i));
                 j--;
             }
         }
