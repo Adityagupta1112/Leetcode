46ecd9c0295047abaad39f7172408fd1f5ee6cf5
Aditya Gupta <adityagupta99904671@gmail.com>
2026-07-12 15:48:22 +0530

    Time: 3 ms (65.54%) | Memory: 9.9 MB (70.86%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    bool helper(string&s, unordered_map<char,int>&mp){
-        
-        for(char & ch:s){
-            if(mp[ch]!=0){
-                return false;
-            }
-        }
-        return true;
-    }
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char,int>mp;
-        int i=0;
-        int j=0;
+        vector<int>vec1(26,0);
+        vector<int>vec2(26,0);
         int m=s1.size();
         int n=s2.size();
-        for(int i=0;i<m;i++){
-            mp[s1[i]]++;
+        if(m>n){
+            return false;
         }
-        while(j<n){
-            while(j-i+1>m){  
-                mp[s2[i]]+=1;
-                i++;
-            }
-            if(mp.count(s2[j])){
-                mp[s2[j]]--;
-            }
-            if(helper(s1,mp)){
+        for(int i=0;i<m;i++){
+            vec1[s1[i]-'a']++;
+            vec2[s2[i]-'a']++;
+        }
+        if(vec1==vec2){
+            return true;
+        }
+        for(int i=m;i<n;i++){
+            vec2[s2[i]-'a']++;
+            vec2[s2[i-m]-'a']--;
+
+            if(vec1==vec2){
                 return true;
             }
-            j++;
         }
         return false;
     }
