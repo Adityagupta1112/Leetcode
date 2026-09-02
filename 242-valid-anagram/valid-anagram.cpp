d23ac72a9c3c8157c12fc4141d198d89982036c3
Aditya Gupta <adityagupta99904671@gmail.com>
2026-09-03 02:18:56 +0530

    Time: 7 ms (18.27%) | Memory: 9.9 MB (25.88%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        if(m!=n){
+            return false;
+        }
+        unordered_map<char,int>mp;
+        for(char ch:s){
+            mp[ch]++;
+        }
+        for(char ch:t){
+            if(!mp.count(ch) || mp[ch]<=0){
+                return false;
+            }
+            mp[ch]--;
+        }
+        return true;
+    }
+};
