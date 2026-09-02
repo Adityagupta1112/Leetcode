800bbca22f0d901f5febbb5de1f89917efec2040
Aditya Gupta <adityagupta99904671@gmail.com>
2026-09-03 02:19:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (49.10%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -7,10 +7,10 @@ public:
             return false;
         }
         unordered_map<char,int>mp;
-        for(char ch:s){
+        for(char &ch:s){
             mp[ch]++;
         }
-        for(char ch:t){
+        for(char &ch:t){
             if(!mp.count(ch) || mp[ch]<=0){
                 return false;
             }
