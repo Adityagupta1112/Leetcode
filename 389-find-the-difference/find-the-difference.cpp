66f3524754fdca077724599b40a961de5e919b8f
Aditya Gupta <adityagupta99904671@gmail.com>
2026-09-02 18:57:42 +0530

    Time: 1 ms (20.24%) | Memory: 9.3 MB (25.68%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char,int>mp;
+        for(char ch:t){
+            if(mp.count(ch)){
+                mp[ch]++;
+            }
+            else{
+                mp[ch]=1;
+            }
+        }
+        for(char ch:s){
+            mp[ch]--;
+        }
+        char ans;
+        for(auto it:mp){
+            if(it.second!=0){
+                ans= it.first;
+                break;
+            }
+        }
+        return ans;
+    }
+};
