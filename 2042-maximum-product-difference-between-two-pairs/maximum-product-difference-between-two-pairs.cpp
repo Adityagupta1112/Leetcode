a08a874abdd50ed30bd8bdb2e9eeaf7f7ff491f4
Aditya Gupta <adityagupta99904671@gmail.com>
2026-09-04 16:59:16 +0530

    Time: 19 ms (71.36%) | Memory: 24.1 MB (71.40%) - LeetSync

---
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int maxProductDifference(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int val1=nums[0]*nums[1];
+        int val2=nums[n-2]*nums[n-1];
+        return val2-val1;
+    }
+};
