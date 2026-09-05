7061671978d1b48cac5d83bca7ef0d3477252751
Aditya Gupta <adityagupta99904671@gmail.com>
2026-09-05 16:17:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (32.12%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -4,16 +4,18 @@ public:
         int n=s.size();
         int score=0;
         int count0=0;
+        int count1=0;
+        for(char ch:s){
+            if(ch=='1'){
+                count1++;
+            }
+        }
         for(int i=0;i<n-1;i++){
             if(s[i]=='0'){
                 count0++;
             }
-            string right=s.substr(i+1);
-            int count1=0;
-            for(int j=i+1;j<n;j++){
-                if(s[j]=='1'){
-                    count1++;
-                }
+            else{
+                count1--;
             }
             score=max(score,count1+count0);
         }
