48fe429fb0396b9eabc94ce64c150b603100e530
Aditya Gupta <adityagupta99904671@gmail.com>
2026-09-13 18:47:25 +0530

    Time: 132 ms (13.25%) | Memory: 142.9 MB (8.58%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> divideArray(vector<int>& nums, int k) {
-        vector<vector<int>>ans;
         sort(begin(nums),end(nums));
         int n=nums.size();
         for(int i=0;i<n;i+=3){
-            vector<int>temp;
-            temp.push_back(nums[i]);
-            for(int j=1;j<3;j++){
-                int diff=nums[i+j]-temp[0];
+            int count=0;
+            for(int j=0;j<3;j++){
+                int diff=nums[i+j]-nums[i];
                 if(diff<=k){
-                    temp.push_back(nums[i+j]);
-                }
-                else{
-                    break;
+                    count++;
                 }
             }
-            if(temp.size()==3){
-                ans.push_back(temp);
-            }
-            else{
+            if(count<3){
                 return {} ;
             }
+        }
+        vector<vector<int>>ans;
+        for(int i=0;i<n;i+=3){
+            vector<int>temp;
+            for(int j=0;j<3;j++){
+                temp.push_back(nums[i+j]);
+            }
+            ans.push_back(temp);
         }
         return ans;
     }
